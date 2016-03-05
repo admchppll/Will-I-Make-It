cd9676906664b240a7f572c03b1e835aff93a624
SWLim <seckwei93@gmail.com>
2016-03-05 07:42:20 +0000

    somehow yelpApi.js was missing

---
+++ server/yelpApi.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var BASE_URL = 'https://api.yelp.com/v2/search/',
+    AUTH = {
+        CONSUMER_KEY:'lPHM9OnJ9UGAdC5MNvhhDg',
+        CONSUMER_SECRET:'_R14pqB0pYmZ9oIswBatGbnA5q8',
+        TOKEN:'nAA2JtgVVOxhp4hf-rIk-FFIe3VTVqB-',
+        TOKEN_SECRET:'5INVOnBSKAgvynudU95QSiCTubo'
+    }
+var request = {
+    Authentication : AUTH
+}
+
+function buildURL(category, addressString){
+    var url = '';
+
+    if (addressString !== ''){
+        url + 'location=' + addressString + '&';
+    }
+    if (category !== ''){
+        url + 'category_filter=' + category;
+    }
+
+    return url = '?' + url;
+};
+
+
+
+module.exports = {
+    buildURL: buildURL
+
+}
